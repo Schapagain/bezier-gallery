f3c8d321be2ae89db25e7dd5eae457abe9d7a1d0
Sandesh <chapagainsandesh88@gmail.com>
2021-02-20 20:25:16 -0500

    add function to display a starry night

--- index.js
+++ index.js
@@ -76,6 +76,89 @@ const lines = function( sketch ) {
 };
 new p5(lines,'lines');
 
+const night = function( sketch ) {
+    let yoff = 0.0; // 2nd dimension of perlin noise
+    sketch.setup = function() {
+        sketch.createCanvas(canvasSize, canvasSize);
+        sketch.frameRate(30);
+    }
+    let chords = [];
+    let stars = [];
+    sketch.draw = function() {
+        sketch.background(0);
+        sketch.fill(255);
+        sketch.stroke(255);
+        // We are going to draw a polygon out of the wave points
+        sketch.beginShape();
+
+        let xoff = 0; // Option #1: 2D Noise
+        // let xoff = yoff; // Option #2: 1D Noise
+
+        // Iterate over horizontal pixels
+        for (let x = 0; x <= sketch.width; x += 10) {
+            // Calculate a y value according to noise, map to
+
+            // Option #1: 2D Noise
+            let y = sketch.map(sketch.noise(xoff, yoff), 0, 1, 0.8 * canvasSize, 0.4 * canvasSize);
+
+            // Option #2: 1D Noise
+            // let y = map(noise(xoff), 0, 1, 200,300);
+
+            // Set the vertex
+            sketch.vertex(x, y);
+            // Increment x dimension for noise
+            xoff += 0.05;
+        }
+        // increment y dimension for noise
+        yoff += 0.01;
+        sketch.vertex(sketch.width, sketch.height);
+        sketch.vertex(0, sketch.height);
+        sketch.endShape(sketch.CLOSE);
+        
+        chords.push(randomChord());
+        if (chords.length > 200) {
+            chords.shift();
+        }
+        for (let chord of chords) {
+            sketch.line(...chord);
+        }
+
+        function randomChord() {
+            // find a random point on a circle
+            let angle1 = sketch.random(0, 2 * Math.PI);
+            let xpos1 = 100 + 50 * sketch.cos(angle1);
+            let ypos1 = 100 + 50* sketch.sin(angle1);
+          
+            // find another random point on the circle
+            let angle2 = sketch.random(0, 2 * Math.PI);
+            let xpos2 = 100 + 50 * sketch.cos(angle2);
+            let ypos2 = 100 + 50 * sketch.sin(angle2);
+          
+            // add a new chord
+            return [xpos1,ypos1,xpos2,ypos2];
+        }
+
+        stars.push(randomStar());
+        if (stars.length > 50) {
+            stars.shift();
+        }
+        for (let star of stars) {
+            sketch.ellipse(...star);
+        }
+
+        function randomStar() {
+            let x = getRandomInt(0,canvasSize);
+            let y = getRandomInt(0,0.4*canvasSize);
+            let size = getRandomInt(1,5);
+            return [x,y,size,size];
+        }
+
+
+    }
+
+};
+new p5(night,'night');
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max + 1);
